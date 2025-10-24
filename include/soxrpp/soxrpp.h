4435953e7d8d42c3ec1f3a9e2418789f46998565
Jason Klein <jklein.64@verizon.net>
2025-10-24 15:21:40 -0400

    feat: naively wrap soxr

--- include/soxrpp/soxrpp.h
+++ include/soxrpp/soxrpp.h
@@ -1,7 +1,34 @@
 #pragma once
 
+#include "soxr.h"
 #include "soxrpp/export.h"
 
+#include <cstddef>
+
 namespace soxrpp {
-SOXRPP_EXPORT bool example();
-}
+
+SOXRPP_EXPORT class SoxResampler {
+  private:
+    soxr_t m_soxr{nullptr};
+
+  public:
+    SoxResampler(double input_rate, double output_rate, unsigned int num_channels, soxr_error_t* err, const soxr_io_spec_t* io_spec,
+                 const soxr_quality_spec_t* quality_spec, const soxr_runtime_spec_t* runtime_spec);
+    ~SoxResampler();
+
+    soxr_error_t process(soxr_in_t in, size_t ilen, size_t* idone, soxr_out_t out, size_t olen, size_t* odone);
+    soxr_error_t set_input_fn(soxr_t resampler, soxr_input_fn_t, void* input_fn_state, size_t max_ilen);
+    size_t output(soxr_t resampler, soxr_out_t data, size_t olen);
+
+    soxr_error_t error();
+    size_t* num_clips();
+    double delay();
+    char const* engine();
+    soxr_error_t clear();
+
+    soxr_error_t oneshot(double input_rate, double output_rate, unsigned num_channels, soxr_in_t in, size_t ilen, size_t* idone,
+                         soxr_out_t out, size_t olen, size_t* odone, const soxr_io_spec_t*, const soxr_quality_spec_t*,
+                         const soxr_runtime_spec_t*);
+};
+
+} // namespace soxrpp
--- src/soxrpp.cpp
+++ src/soxrpp.cpp
@@ -1,36 +1,56 @@
 #include "soxrpp/soxrpp.h"
-#include "soxr.h"
 
+#include "soxrpp.h"
 #include <array>
 
 namespace soxrpp {
 
-const std::array<float, 48> in = {/* Input: 12 cycles of a sine wave with freq. = irate/4 */
-                                  0, 1, 0, -1, 0, 1, 0, -1, 0, 1, 0, -1, 0, 1, 0, -1, 0, 1, 0, -1, 0, 1, 0, -1,
-                                  0, 1, 0, -1, 0, 1, 0, -1, 0, 1, 0, -1, 0, 1, 0, -1, 0, 1, 0, -1, 0, 1, 0, -1};
-
-bool example() {
-    double irate = 1; /* Default to interpolation */
-    double orate = 2; /* by a factor of 2. */
-
-    size_t olen = (size_t)(in.size() * orate / irate + .5); /* Assay output len. */
-    float* out = (float*)malloc(sizeof(*out) * olen);       /* Allocate output buffer. */
-    size_t odone;
-
-    soxr_error_t error = soxr_oneshot(irate, orate, 1,            /* Rates and # of chans. */
-                                      in.data(), in.size(), NULL, /* Input. */
-                                      out, olen, &odone,          /* Output. */
-                                      NULL, NULL, NULL);          /* Default configuration.*/
-
-    unsigned i = 0; /* Print out the resampled data, */
-    while (i++ < odone)
-        printf("%5.2f%c", out[i - 1], " \n"[!(i & 7) || i == odone]);
-    printf("%-26s %s\n", "prog", soxr_strerror(error)); /* and reported result. */
-
-    /* Library version check: */
-    printf("runtime=%s API=" SOXR_THIS_VERSION_STR "\n", soxr_version());
-
-    free(out); /* Tidy up. */
-    return !!error;
+SoxResampler::SoxResampler(double input_rate, double output_rate, unsigned int num_channels, soxr_error_t* err,
+                           const soxr_io_spec_t* io_spec, const soxr_quality_spec_t* quality_spec,
+                           const soxr_runtime_spec_t* runtime_spec) {
+    m_soxr = soxr_create(input_rate, output_rate, num_channels, err, io_spec, quality_spec, runtime_spec);
 }
+
+SoxResampler::~SoxResampler() {
+    soxr_delete(m_soxr);
+}
+
+soxr_error_t SoxResampler::process(soxr_in_t in, size_t ilen, size_t* idone, soxr_out_t out, size_t olen, size_t* odone) {
+    return soxr_process(m_soxr, in, ilen, idone, out, olen, odone);
+}
+
+soxr_error_t SoxResampler::set_input_fn(soxr_t resampler, soxr_input_fn_t input_fn, void* input_fn_state, size_t max_ilen) {
+    return soxr_set_input_fn(m_soxr, input_fn, input_fn_state, max_ilen);
+}
+
+size_t SoxResampler::output(soxr_t resampler, soxr_out_t data, size_t olen) {
+    return soxr_output(m_soxr, data, olen);
+}
+
+soxr_error_t SoxResampler::error() {
+    return soxr_error(m_soxr);
+}
+
+size_t* SoxResampler::num_clips() {
+    return soxr_num_clips(m_soxr);
+}
+
+double SoxResampler::delay() {
+    return soxr_delay(m_soxr);
+}
+
+char const* SoxResampler::engine() {
+    return soxr_engine(m_soxr);
+}
+
+soxr_error_t SoxResampler::clear() {
+    return soxr_clear(m_soxr);
+}
+
+soxr_error_t SoxResampler::oneshot(double input_rate, double output_rate, unsigned num_channels, soxr_in_t in, size_t ilen,
+                                   size_t* idone, soxr_out_t out, size_t olen, size_t* odone, const soxr_io_spec_t* io_spec,
+                                   const soxr_quality_spec_t* quality_spec, const soxr_runtime_spec_t* runtime_spec) {
+    return soxr_oneshot(input_rate, output_rate, num_channels, in, ilen, idone, out, olen, odone, io_spec, quality_spec, runtime_spec);
+}
+
 } // namespace soxrpp
